96db04dfe113fa8e9f8d0fa4975d96f91b38037c
RamR3R <sendtoramsundar@gmail.com>
2023-01-14 11:37:33 +0530

    The Optimised prime code

--- Prime.js
+++ Prime.js
@@ -1,10 +1,11 @@
 let count=0;
-let num=23;
-    for(let i=1;i<=num;i++){
+let num=83;
+    for(let i=2;i<=Math.sqrt(num);i++){
         if(num%i==0){
             count++;
+	    break;	
         }
     }
-    if(count<=2){
+    if(count == 0){
         console.log("Yes");
 }else console.log("No");
