0069e4d16154452065696f699a55f11b2343f683
Lakshmiguru12 <lakshmisr1996@gmail.com>
2023-01-14 11:23:22 +0530

    Prime numbers

---
+++ Prime.js
@@ -0,0 +1,10 @@
+let count=0;
+let num=23;
+    for(let i=1;i<=num;i++){
+        if(num%i==0){
+            count++;
+        }
+    }
+    if(count<=2){
+        console.log("Yes");
+}else console.log("No");
